775e776963abd43fcc64e0dc20179da55348af65
MREugeneJ7 <32437774+MREugeneJ7@users.noreply.github.com>
2024-01-17 23:39:01 +0000

    Done

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,9 +6,21 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  // Write the code 
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: generatePrompt(req.body.words),
+    temperature: 0.6,
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(animal) {
-  // Write the code
+function generatePrompt(words) {
+  return `Suggest two places to go on vacation based on two words.
+
+Words: Surf Island
+Places: Hawaii, Tavarua
+Words: Mountain Buda
+Places: Tibet Nepal
+Words: ${words}
+Places:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,52 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  // Write the code
+  const [firstWordInput, setFirstWordInput] = useState("");
+  const [secondWordInput, setSecondWordInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ words: firstWordInput + " " + secondWordInput }),
+    });
+    const data = await response.json();
+    setResult(data.result);
+    setFirstWordInput("");
+    setSecondWordInput("");
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>OpenAI Quickstart</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h3>Tell me where to go</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="First word"
+            placeholder="Enter a word related to a place you'd want to go"
+            value={firstWordInput}
+            onChange={(e) => setFirstWordInput(e.target.value)}
+          />
+          <input
+            type="text"
+            name="Second word"
+            placeholder="Enter another word related to a place you'd want to go"
+            value={setFirstWordInput}
+            onChange={(e) => setSecondWordInput(e.target.value)}
+          />
+          <input type="submit" value="Generate Places" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
 }
